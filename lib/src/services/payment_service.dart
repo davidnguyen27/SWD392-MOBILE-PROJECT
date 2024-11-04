d8b4bb44c43369f7e36749f0a4eb5efeb35c4526
Ka Ka Lưu <kacode357@gmail.com>
2024-11-04 21:53:45 +0700

    fix date payment

--- lib/src/services/payment_service.dart
+++ lib/src/services/payment_service.dart
@@ -21,18 +21,10 @@ class PaymentService {
       }),
     );
 
-    print(
-        'API Response Status Code: ${response.statusCode}'); // Debug: Trạng thái phản hồi
-    print(
-        'API Response Body: ${response.body}'); // Debug: Nội dung phản hồi từ API
-
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-
       List<dynamic> paymentList = data['data']['pageData'] ?? [];
-      print(
-          'Parsed payment list: $paymentList'); // Debug: In danh sách payment đã phân tích
-
+      print('Parsed payment list (after assignment): $paymentList');
       return paymentList.map((json) => Payment.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load payments');
